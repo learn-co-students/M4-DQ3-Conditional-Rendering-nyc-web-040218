597cfa1bbe31ee04bce5acd239b4671220819753
briecodes <hello@brie.work>
2018-06-06 11:39:33 -0400

    lab completed.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -15,19 +15,19 @@ const MenuBar = (props) => {
 
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a className="item active" id="profile" onClick={props.fun}>
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a className="item" id="photo" onClick={props.fun}>
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a className="item" id="cocktail" onClick={props.fun}>
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a className="item" id="pokemon" onClick={props.fun}> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
